95ddf5f18a024fee4b5216c42dd6ea90021e6fc0
Yosua Nicolaus <cryodawn257@gmail.com>
2022-03-06 11:27:55 +0700

    implement convert to 2d array and horizontal check

--- tictactoe.js
+++ tictactoe.js
@@ -38,7 +38,7 @@ const Game = (() => {
     _turn++;
     Gameboard.fill(index, symbol);
     Gameboard.display();
-    if (_turn >= 3) checkStatus(symbol);
+    if (_turn >= 5) checkStatus(symbol);
   };
 
   const checkValidity = (index) => {
@@ -46,8 +46,33 @@ const Game = (() => {
   };
 
   const checkStatus = (symbol) => {
+    console.log("checking status");
     const board = Gameboard.display();
+    let boardIndex = 0;
     // TODO: implement check for winner
+    // convert 1d array to 2d array
+    const newBoard = new Array(3);
+    for (let i = 0; i < 3; i++) {
+      newBoard[i] = [];
+      for (let j = 0; j < 3; j++) {
+        newBoard[i][j] = board[boardIndex++];
+      }
+    }
+    console.table(newBoard);
+
+    let key = new Array(3);
+    function checkKey() {
+      if (key[0] === key[1] && key[0] === key[2]) {
+        console.log(symbol + " PLAYER WIN!!! YOOO");
+      }
+    }
+    // check for same i
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        key[j] = newBoard[i][j];
+      }
+      checkKey();
+    }
   };
 
   const playTurn = (index) => {
